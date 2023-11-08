20094368d39356c0c6bc7d94e4f0d63e7c4b87ba
tjoeun <tjoeun@host.docker.internal>
2023-11-08 17:43:59 +0900

    mapper

--- muinprojact/src/main/java/com/example/demo/mapper/MemberMapper.java
+++ muinprojact/src/main/java/com/example/demo/mapper/MemberMapper.java
@@ -1,9 +1,11 @@
 package com.example.demo.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.command.MemberVO;
 
+@ResponseBody
 @Mapper
 public interface MemberMapper {
 	public int register(MemberVO vo);	//회원가입
--- muinprojact/src/main/java/com/example/demo/service/MemberService.java
+++ muinprojact/src/main/java/com/example/demo/service/MemberService.java
@@ -1,7 +1,10 @@
 package com.example.demo.service;
 
+import org.springframework.stereotype.Service;
+
 import com.example.demo.command.MemberVO;
 
+@Service
 public interface MemberService {
 	public int register(MemberVO vo);
 }
