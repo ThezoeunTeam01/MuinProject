e4b1000a58303ad6ac92eb510da14da668c8fd15
STARella01 <seongee2rum@gmail.com>
2023-11-21 17:31:00 +0900

    업로드 페이지 클래스 수정 및 file.js-> board.js / member.js 나눔 처리 _ 20231121 강한별

--- muinprojact/src/main/resources/static/js/script.js
+++ muinprojact/src/main/resources/static/js/script.js
@@ -53,7 +53,7 @@ $(document).ready(function() {
              $("#pwCheck").focus();                 
       }                                            
    });
-    //id체크
+    // id 체크
      $('#idCheck').click(function(){
    
          var id = $("#id").val();
@@ -88,7 +88,7 @@ $(document).ready(function() {
    });
 });
        
-   // joinCheck 함수
+   // regiCheck 함수
    function regiCheck() {
       if (!$("#id").attr("readonly")) {
           alert("아이디 중복체크를 해야 합니다.");
@@ -116,7 +116,8 @@ $(document).ready(function() {
               $("#muinRegiForm").submit();
       }
    };
-   // joinCheck 함수
+   
+   // memberUpdateCheck 함수
    function memberUpdateCheck() {
 	  if ($("#pw").val().length < 1) {
           alert("비밀번호를 입력하세요!");
@@ -139,4 +140,35 @@ $(document).ready(function() {
               $("#memberUpdateForm").submit();
       }
    };
+   
+   
+   // boardRegiCheck 함수 - 임시 설정
+   function boardRegiCheck() {
+      if (!$("#id").attr("readonly")) {
+          alert("아이디 중복체크를 해야 합니다.");
+      } else if ($("#pw").val().length < 1) {
+          alert("비밀번호를 입력하세요!");
+      } else if ($("#pw").val() !== $("#pwCheck").val()) {
+          alert("비밀번호 확인란을 확인해 주세요!");
+          $("#pwCheck").focus();
+      } else if ($("#name").val().length < 1) {
+          alert("이름을 입력하세요!");
+          $("#name").focus();
+      } else if ($("#birth").val().length < 8) {
+          alert("생년월일을 입력하세요!");
+          $("#birth").focus();
+      } else if ($("#address").val() === "") {
+          alert("프로필 파일을 업로드 하세요!");
+          $("#address").focus();
+      } else if ($("#phone").val() === "") {
+          alert("번호를 입력하세요!");
+          $("#phone").focus();
+      } else if ($("#profile").val() === "") {
+          alert("프로필 파일을 업로드 하세요!");
+          $("#profile").focus();
+      }else  if (confirm("회원 가입을 하시겠습니까?")) {
+              $("#muinRegiForm").submit();
+      }
+   };     
+   
  
